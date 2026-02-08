e35ef355fdfe1bdac68a56e43a123230961cb0ff
Danny <daniel18700@outlook.com>
2026-02-07 21:58:46 -0800

    asm comments

--- Lab1/lab1.asm
+++ Lab1/lab1.asm
@@ -16,7 +16,7 @@ _start:
 # Print input array
 ##################################################
 la a0, msg_input              # load message address
-li a7, 4                      # print string syscall
+li a7, 4                      # print string syscall to a7 (4 is for printing string)
 ecall
 la t0, array                  # load array start
 li t1, 5                      # number of elements
@@ -26,7 +26,7 @@ jal print_loop
 # Call bubble sort
 ##################################################
 la a0, array                  # array pointer arg
-li a1, 5                      # length arg
+li a1, 5                      # loads immediate 5 (length) into a1 to use during bubble sort 
 call bubble_sort
 ##################################################
 # Print sorted
@@ -38,7 +38,7 @@ la t0, array                  # load array again
 li t1, 5                      # element count
 li t2, 0                      # index = 0
 jal print_loop
-li a7, 10                     # exit program syscall
+li a7, 10                     # exit program syscall (finishes code after printing "print done")
 ecall
 ############################################################
 # bubble_sort
@@ -46,7 +46,7 @@ ecall
 bubble_sort:
 li a6, 1                      # constant 1
 sub t0, a1, a6                # n - 1 (outer limit)
-blez t0, done                 # skip if empty/one
+blez t0, done                 # skip if empty/one (<= 0)
 outer_loop:
 li t1, 0                      # inner index j = 0
 mv t2, a0                     # current element pointer
@@ -54,7 +54,7 @@ li t6, 0                      # swap flag = false
 inner_loop:
 lw t3, 0(t2)                  # load current element
 lw t4, 4(t2)                  # load next element
-ble t3, t4, no_swap           # skip if in order
+ble t3, t4, no_swap           # skip if in order (t3 <= t4)
 sw t4, 0(t2)                  # swap: write next here
 sw t3, 4(t2)                  # swap: write current there
 li t6, 1                      # set swap happened
@@ -62,9 +62,9 @@ no_swap:
 addi t2, t2, 4                # move to next element
 addi t1, t1, 1                # j++
 blt t1, t0, inner_loop        # repeat inner loop
-beqz t6, done                 # done if no swaps
+beqz t6, done                 # done if no swaps (t6 is 0 or 1 if a swap occured)
 sub t0, t0, a6                # outer limit--
-bgtz t0, outer_loop           # repeat outer loop
+bgtz t0, outer_loop           # repeat outer loop (if t0 > 0)
 done:
 ret
 ############################################################
@@ -73,16 +73,16 @@ ret
 print_loop:
 beq t2, t1, print_done        # done when i == count
 lw a0, 0(t0)                  # load element to print
-li a7, 1                      # print integer syscall
+li a7, 1                      # print integer syscall (1 is for printing integer)
 ecall
 la a0, space                  # load space string
 li a7, 4                      # print string
 ecall
-addi t0, t0, 4                # next array element
-addi t2, t2, 1                # i++
+addi t0, t0, 4                # next array element (increments 4 bytes to next int)
+addi t2, t2, 1                # i++ to the t2 counter
 j print_loop
 print_done:
 la a0, newline                # load newline
 li a7, 4                      # print string
 ecall
-ret
+ret                           # returns to jump address
