793ebb1fb3b628da225a85dc01c121bc36b3d527
Manjot Uppal <157860173+muppal-merced@users.noreply.github.com>
2026-03-02 04:30:30 +0000

    updated:

--- Lab3/daniel_sanchez_manjot_uppal.py
+++ Lab3/daniel_sanchez_manjot_uppal.py
@@ -43,9 +43,30 @@ INSTRUCTION_MAP = {
     "1100111": {
         (0,): "jalr", #funct3 = 0, no funct 7
     },
-    # ##########################
-    # Need s,SB, UJ types
-    # ##########################
+
+    # S-type store (opcode 0100011)
+     "0100011": {
+         (0,): "sb", #funct3 = 0, no
+         (1,): "sh", #funct3 = 1, funct7 not used
+         (2,): "sw", #funct3 = 2, funct7 not used
+
+     },
+
+    # SB-type branch (opcode 1100011)
+    "1100011": {
+        (0,): "beq", #funct3 = 0, no funct7
+        (1,): "bne", #funct3 = 1, no funct7
+        (4,): "blt", #funct3 = 4, no funct7
+        (5,): "bge", #funct3 = 5, no funct7
+        (6,): "bltu", #funct3 = 6, no funct7
+        (7,): "bgeu", #funct3 = 7, no funct7
+
+    },
+
+    # UJ-type jump (opcode 1101111)
+    "1101111": {
+        (0,): "jal", #funct3 = 0, no funct7
+    },
 }
 def get_immediate(bits, opcode):
     #Using opcode, take the immediate bits from the instruction and convert to signed decimal.
@@ -53,10 +74,21 @@ def get_immediate(bits, opcode):
     if opcode in ("0010011", "0000011", "1100111"):
         imm_bits = bits[0:12]
         n = 12 #12 bits for immediate
-    # ##########################
-    # Need s,SB, UJ types
-    # ##########################
 
+    # S-type store
+    elif opcode == "0100011": 
+        imm_bits = bits[0:7] + bits[25:32] 
+        n = 12 # 12 bits for immediate in S-type
+
+    # SB-type branch
+    elif opcode == "1100011": 
+        imm_bits = bits[0] + bits[20:25] + bits[7:12] + bits[31:32]
+        n = 13 # 13 bits for immediate in SB-type
+
+    # UJ-type jump
+    elif opcode == "1101111":
+        imm_bits = bits[0] + bits[12:20] + bits[11] + bits[1:11] + '0' 
+        n = 21 # 21 bits for immediate in UJ-type
     else:
         return None, 0 #if opcode is not in the dictionary, return None and 0
     #convert binary to decimal
@@ -89,10 +121,20 @@ def decode_instruction(binary_instruction):
         ins_type = "I"
     elif opcode == "1100111":
         ins_type = "I"
-   
-    # ##########################
-    # Need s,SB, UJ types
-    # ##########################
+    elif opcode == "0010111":
+        ins_type = "U"
+    elif opcode == "0100011":
+        ins_type = "S"
+    elif opcode == "0110111":
+        ins_type = "U"
+    elif opcode == "0111011":
+        ins_type = "R"
+    elif opcode == "1100011":
+        ins_type = "SB"
+    elif opcode == "1101111":
+        ins_type = "UJ"
+    elif opcode == "1110011":
+        ins_type = "I" #ecall and ebreak are I-type with funct3 = 0 and funct7 = 0 or 1
     else:
         ins_type = "Unknown"
     type = ins_type
@@ -106,13 +148,12 @@ def decode_instruction(binary_instruction):
     rs1_int = int(rs1_bits, 2)
     rs2_int = int(rs2_bits, 2)
     rd_int = int(rd_bits, 2)
-    # ##########################
-    # Need s,SB, UJ types
-    # ##########################
    
     if opcode in ("0100011", "1100011", "1101111"):
-        instruction = "(need S, SB, UJ)"
-        immediate_val, imm_bits = None, 0
+        type_map = INSTRUCTION_MAP[opcode]
+        key = (funct3_int,)  # S-type, SB-type, and UJ-type only use funct3 for instruction mapping
+        instruction = type_map.get(key, "Unknown")  #"Unknown" if key is not found
+        immediate_val, imm_bits = get_immediate(binary_instruction, opcode)
     else:
         type_map = INSTRUCTION_MAP[opcode]
         if opcode in ("0110011", "0010011"):  # R-type and I-type ALU: need (funct3, funct7)
@@ -144,10 +185,28 @@ def main():
         print("Rd: x" + str(rd_int))
         if immediate_val is not None:
             print("Immediate:", format_immediate(immediate_val, imm_bits))
-    # ##########################
-    # Need s,SB, UJ types
-    # ##########################
 
+    elif type == "S":
+        print ("Rs1: x" + str(rs1_int))
+        print ("Rs2: x" + str(rs2_int))
+        if immediate_val is not None:
+            print ("Immediate:", format_immediate(immediate_val, imm_bits))
+
+    elif type == "SB":
+        print ("Rs1: x" + str(rs1_int))
+        print ("Rs2: x" + str(rs2_int))
+        if immediate_val is not None :
+            print ("Immediate:", format_immediate(immediate_val, imm_bits))
+    
+    elif type == "UJ":
+        print ("Rd: x" + str(rd_int))
+        if immediate_val is not None:
+            print ("Immediate:", format_immediate(immediate_val, imm_bits))
+
+    elif type == "U":
+        print ("Rd: x" + str(rd_int))
+        if immediate_val is not None:
+            print ("Immediate:", format_immediate(immediate_val, imm_bits))
 
 
 if __name__ == "__main__":
