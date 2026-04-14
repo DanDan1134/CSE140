616fab735b7e3ef8dfd1faa2319d042416de19b8
Danny <daniel18700@outlook.com>
2026-04-13 20:44:01 -0700

    added more comments to writebackl

--- Project/cse140_cpu/writeback.py
+++ Project/cse140_cpu/writeback.py
@@ -2,14 +2,12 @@
 
 Writeback stage — MemToReg mux, write rf when RegWrite; increment total_clock_cycles
 
-every finished instruction (PDF). Trace printing lives in main.
+writes the data to the register file
+
 
 """
 
 import state
-
-
-
 
 
 def writeback():
@@ -18,34 +16,16 @@ def writeback():
 
     state.total_clock_cycles += 1
 
-
-
     if state.reg_write == 0: # no register update this cycle
-
         return
-
-
-
     if state.rd == 0: # x0 is not writable so we just return
-
         return  
-
-
-
     if state.mem_to_reg == 1: # lw instruction: use memory data
-
         data = state.mem_read_data
-
     else: # R-type and I-type instructions: use ALU result
-
         data = state.alu_result
-
-
-
     state.write_back_data = data # for logging and debugging
-
     state.rf[state.rd] = data # write the data to the register file
-
     state.rf[0] = 0  # keep x0 hard-wired to 0 because it is always 0
 
 
